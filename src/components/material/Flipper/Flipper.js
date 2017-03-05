276c175d13362cfb84048d29dbb81ed7e7916f15
yuechuan <yuechuan20@gmail.com>
2017-03-04 19:31:44 -0500

    added a flipper component

---
+++ src/components/material/Flipper/Flipper.js
@@ -0,0 +1,32 @@
+/**
+ * multi-page vertical material themed flipper.
+ *  <Flipper>
+ *    <article/>
+ *    <article/>
+ *  </Flipper>
+ **/
+import React from 'react'
+import './flipper.css'
+
+class Flipper extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    const {index = 0, className, face = false, style} = this.props
+
+    return (
+      <div
+        className={'flip-container ' + className}
+        style={style}  
+      >
+        <div className={`flipper ${face ? 'flip1' : 'flip2'}`}>
+          {this.props.children[index]}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Flipper
