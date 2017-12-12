46177407cf74af60113718e078d6b6abeaf38071
Chen Yuechuan-XJQW46 <Yuechuan.Chen@motorolasolutions.com>
2017-12-11 21:48:41 -0500

    Updated aboutme page

--- src/components/material/AboutPage.js
+++ src/components/material/AboutPage.js
@@ -1,45 +1,60 @@
-import React from 'react'
-import profImg from '../../../public/img/profile.jpg'
-import Markdown from 'react-remarkable'
-import './AboutPage.css'
+import React from "react";
+import profImg from "../../../public/img/profile.jpg";
+import Markdown from "react-remarkable";
+import "./AboutPage.css";
 
 function AboutPage(props) {
-
   return (
-    <div >
-
-      <div className='about-img-div'>
-        
-        <img src={profImg}
-          alt={"profile"}
-          className='img-prof-responsive' />
-
+    <div>
+      <div className="about-img-div">
+        <img src={profImg} alt={"profile"} className="img-prof-responsive" />
       </div>
 
       {generateContent()}
-
     </div>
-  )
+  );
 }
 
 function generateContent() {
   return (
-    <div className='about-page-main'>
-      <p>Hi, I'm' Mike.</p>
+    <div className="about-page-main">
+      <p>
+        Hi! My name is Mike. Graduated from [McGill](https://www.mcgill.ca), I
+        am a security engineer by trade, and a software engineer at heart.
+      </p>
       <Markdown>
-        Recently graduated from [McGill University](https://www.mcgill.ca) with a degree of Bachelor of Software Engineering. I am a learner, explorer and an aspiring dancer at heart.
+        For the past year, I have joined Motorola Solutions as a Security
+        engineer specialized in application security and Golang development.
+        There, I have the pleasure to contribute to projects related to
+        container and mobile security.
+      </Markdown>
+      <Markdown>
+        I enjoy learning and building my personal projects while I've
+        got time on hand. Recently, I have obtained my [AWS Solution Architect
+        Associate
+        Certificate](https://aws.amazon.com/certification/certified-solutions-architect-associate/).
+        I am also in the process of learning to build deploy and scale cloud
+        applications.
       </Markdown>
 
-      <p>Seeking to be inspired, to envision the unlikely, to work hard for things that are worth it, and to be surrounded by those that works hard and plays hard.</p>
-
-      <p>Moved from China to Montreal, Montreal to New York. I am seeking for an exciting opportunity to begin my career in California.</p>
-
       <Markdown>
-        Come and say Hi via the "Contact" tab or on [Facebook](https://www.facebook.com/chen.yuechuan)!
+        I enjoy traveling, dancing and building side projects (currently working
+        on a react-native app).
       </Markdown>
 
+      <p>
+        Moved from China to Montreal, Montreal to New York and New York to
+        Chicago. I am seeking an exciting opportunity to advance my career.
+      </p>
+
+      <Markdown>
+        If you want to hear more about me, check out my [LinkedIn
+        profile](https://www.linkedin.com/in/yuechuan-mike-chen-995b9b54/) or
+        send me an [email](mailto:yuechuan20@gmail.com).
+      </Markdown>
+      <Markdown>Chao</Markdown>
     </div>
-  )
+  );
 }
 
-export default AboutPage
+export default AboutPage;
